a157b96d50427e9cc0a853ccc27e7e2b1c99df8c
aakashnarla <aakash.narla.rao@gmail.com>
2022-07-29 17:12:36 +0530

    Added basic support for Movies API

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MoviesService {
+  constructor(private http : HttpClient) {}
 
-  constructor() { }
+  getMovies() {
+    return this.http.get('https://api.themoviedb.org/3/movie/upcoming?api_key=0ba433fcad9884f3878175ea24ab9a84')
+  }
 }

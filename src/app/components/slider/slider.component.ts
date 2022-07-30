673b616f0a9f9267c7283207393421c66298864f
aakashnarla <aakash.narla.rao@gmail.com>
2022-07-30 19:05:27 +0530

    Updated slider for home page

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,14 +1,27 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Movie } from '../../models/movie';
+import { state, style, trigger, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'slider',
   templateUrl: './slider.component.html',
-  styleUrls: ['./slider.component.scss']
+  styleUrls: ['./slider.component.scss'],
+  animations: [
+    trigger('slideFade', [
+      state('void', style({ opacity: 0 })),
+      transition('void <=> *', [animate('1s')])
+    ])
+  ]
 })
 export class SliderComponent implements OnInit {
   @Input() items: Movie[] = [];
+
+  currentSlideIndex: number = 1;
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    setInterval(() => {
+      this.currentSlideIndex ==++this.currentSlideIndex % this.items.length;
+    }, 5000)
+  }
 }
